5a2d355729f31edef7e8fe8d8ef4f388fdef7cc2
joe-badawi <joebadawi9@gmail.com>
2022-09-19 13:34:50 +0300

    added side menu icons

--- app/EService.php
+++ app/EService.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Helpers\Helper;
 use Illuminate\Database\Eloquent\Model;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract; use Astrotomic\Translatable\Translatable;
 
@@ -14,6 +15,18 @@ class EService extends Model  implements TranslatableContract
     protected $guarded = ['id'];
 
     protected $hidden = ['translations'];
+
+    public $appends = ['side_menu_icon_full_path'];
+
+    public function getSideMenuIconFullPathAttribute()
+    {
+        if (isset($this->side_menu_icon)) {
+            $side_menu_icon_full_path = Helper::fullPath($this->side_menu_icon);
+            return $side_menu_icon_full_path;
+        } else {
+            return null;
+        }
+    }
 
 
     public $translatedAttributes = ["title", "excerpt", "description", "terms_text", "benefits_text", "documents_text", "fees_text", "label"];
