e380ba5c9088f8e6dee5c97827db1479b5fd0e12
joe-badawi <joebadawi9@gmail.com>
2022-09-26 17:52:23 +0300

    fixed news events

--- app/Http/Controllers/NewsEventsController.php
+++ app/Http/Controllers/NewsEventsController.php
@@ -6,6 +6,7 @@ use App\Event;
 use App\NewsEventsSetting;
 use App\NewsList;
 use App\PublicationsList;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class NewsEventsController extends Controller
@@ -15,7 +16,9 @@ class NewsEventsController extends Controller
 
         $news = NewsList::orderBy("date", "desc")->get();
 
-        $events = Event::orderBy("date", "desc")->get();
+        $previous_events = Event::orderBy("date", "desc")->where("date", "<=", Carbon::now())->get();
+
+        $upcoming_events = Event::orderBy("date", "desc")->where("date", ">", Carbon::now())->get();
 
         $publications = PublicationsList::orderBy("date", "desc")->get();
 
