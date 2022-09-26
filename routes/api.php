4b7284b802768735b37877db40177826f4951b88
joe-badawi <joebadawi9@gmail.com>
2022-09-26 15:33:40 +0300

    404 apis

--- routes/api.php
+++ routes/api.php
@@ -21,13 +21,7 @@ use App\Http\Controllers\CircularsController;
 use App\Http\Controllers\NewsEventsController;
 use App\Http\Controllers\MailchimpController;
 use App\Http\Controllers\ContactController;
-
-
-
-
-
-
-
+use App\Http\Controllers\NotFoundController;
 
 /*
 |--------------------------------------------------------------------------
@@ -77,8 +71,8 @@ Route::middleware('locale')->prefix('{locale}')->group(function () {
     Route::get('/contact-data', [ContactController::class, 'index']);
     Route::get('/directory', [DirectoryController::class, 'index']);
     Route::get('/directory/{slug}', [DirectoryController::class, 'singleDirectory']);
-    Route::get('/not-found', [DirectoryController::class, 'index']);
-    Route::get('/searchDirectory/{slug}', [NotFoundController::class, 'searchDirectory']);
+    Route::get('/not-found', [NotFoundController::class, 'index']);
+    Route::get('/searchDirectory/{slug}', [DirectoryController::class, 'searchDirectory']);
 
 
     Route::post('/newsletter', [MailchimpController::class, 'index']);
