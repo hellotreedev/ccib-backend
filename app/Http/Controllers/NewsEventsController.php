665da5aaab9f8f5c1d3c88d3ac91675122e388ce
joe-badawi <joebadawi9@gmail.com>
2022-09-26 15:54:10 +0300

    added news, events, pub, and disclaimer title

--- app/Http/Controllers/NewsEventsController.php
+++ app/Http/Controllers/NewsEventsController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Event;
 use App\NewsEventsSetting;
+use App\NewsList;
+use App\PublicationsList;
 use Illuminate\Http\Request;
 
 class NewsEventsController extends Controller
 {
     public function index() {
         $settings = NewsEventsSetting::first();
-        return compact('settings');
+
+        $news = NewsList::orderBy("date", "desc")->get();
+
+        $events = Event::orderBy("date", "desc")->get();
+
+        $publications = PublicationsList::orderBy("date", "desc")->get();
+
+        return compact('settings', 'news', 'events', 'publications');
     }
 }
