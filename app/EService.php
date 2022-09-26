e2b0d4e0845014c577ddfbf2b43cf3aca79d8005
hellotree.dev <support@hellotree.dev>
2022-09-26 09:36:30 +0300

    added fields to tables

--- app/EService.php
+++ app/EService.php
@@ -2,7 +2,6 @@
 
 namespace App;
 
-use App\Helpers\Helper;
 use Illuminate\Database\Eloquent\Model;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract; use Astrotomic\Translatable\Translatable;
 
@@ -14,23 +13,7 @@ class EService extends Model  implements TranslatableContract
 
     protected $guarded = ['id'];
 
-    protected $hidden = ['translations'];
+    public $translatedAttributes = ["title","excerpt","description","terms_text","benefits_text","documents_text","fees_text","label","section1_title","section1_text","section2_content","section3_title","section3_subtitle","section4_content","section5_content","download_btn","btn_pdf","btn_url"];
 
-    public $appends = ['side_menu_icon_full_path'];
-
-    public function getSideMenuIconFullPathAttribute()
-    {
-        if (isset($this->side_menu_icon)) {
-            $side_menu_icon_full_path = Helper::fullPath($this->side_menu_icon);
-            return $side_menu_icon_full_path;
-        } else {
-            return null;
-        }
-    }
-
-
-    public $translatedAttributes = ["title", "excerpt", "description", "terms_text", "benefits_text", "documents_text", "fees_text", "label", 'section1_title', 'section1_text', 'section2_content',  'section3_title', 'section3_subtitle', 'section4_content','section5_content', 'download_btn', 'btn_pdf', 'btn_url'];
-
-		public function boxes() { return $this->belongsToMany('App\EservicesBox', 'eservices_box_e_service', 'e_service_id', 'eservices_box_id')->orderBy('eservices_box_e_service.ht_pos'); } 
-
+	public function boxes() { return $this->belongsToMany('App\EservicesBox', 'eservices_box_e_service', 'e_service_id', 'eservices_box_id')->orderBy('eservices_box_e_service.ht_pos'); } 
 }
