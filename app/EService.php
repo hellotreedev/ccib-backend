654f0e85114369c53f881fd8c47cc6c582c2a58d
hellotreedigital <info@hellotree.co>
2022-09-28 09:53:47 +0300

    added full paths to icons in eservices and pdf label

--- app/EService.php
+++ app/EService.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Helpers\Helper;
 use Illuminate\Database\Eloquent\Model;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract; use Astrotomic\Translatable\Translatable;
 
@@ -16,6 +17,28 @@ class EService extends Model  implements TranslatableContract
     protected $hidden = ['translations'];
 
     public $translatedAttributes = ["title","excerpt","description","terms_text","benefits_text","documents_text","fees_text","label","section1_title","section1_text","section2_content","section3_title","section3_subtitle","section4_content","section5_content","download_btn","btn_pdf","btn_url"];
+
+    public $appends = ['side_menu_icon_full_path', 'icon'];
+
+    public function getSideMenuIconFullPathAttribute()
+    {
+        if (isset($this->side_menu_icon)) {
+            $side_menu_icon_full_path = Helper::fullPath($this->side_menu_icon);
+            return $side_menu_icon_full_path;
+        } else {
+            return null;
+        }
+    }
+
+    public function getIconFullPathAttribute()
+    {
+        if (isset($this->icon)) {
+            $icon_full_path = Helper::fullPath($this->icon);
+            return $icon_full_path;
+        } else {
+            return null;
+        }
+    }
 
 	public function boxes() { return $this->belongsToMany('App\EservicesBox', 'eservices_box_e_service', 'e_service_id', 'eservices_box_id')->orderBy('eservices_box_e_service.ht_pos'); } 
 }
--- app/ServicesList.php
+++ app/ServicesList.php
@@ -4,11 +4,12 @@ namespace App;
 
 use App\Helpers\Helper;
 use Illuminate\Database\Eloquent\Model;
-use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract; use Astrotomic\Translatable\Translatable;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
 
 class ServicesList extends Model  implements TranslatableContract
 {
-	use Translatable;
+    use Translatable;
 
     protected $table = 'services_list';
 
@@ -16,13 +17,13 @@ class ServicesList extends Model  implements TranslatableContract
 
     protected $hidden = ['translations'];
 
-    public $appends = ["image_full_path","icon_full_path","gold_icon_full_path", "phone_icon_full_path", "fax_icon_full_path", "ext_icon_full_path", "mail_icon_full_path", "download_icon_full_path", "pdf_full_path"];
-    
-        public $translatedAttributes = ["title","excerpt","learn_more","single_page_title","about_title","about_description","publications_title","services_title","contact_us_title","pdf","contact_us","phone_text","fax_text","ext_text","download_pdf","different_section_image","different_page_title","different_section_description","different_perform_transactions","different_transactions_pdf","different_contact_title"];
+    public $appends = ["image_full_path", "icon_full_path", "gold_icon_full_path", "phone_icon_full_path", "fax_icon_full_path", "ext_icon_full_path", "mail_icon_full_path", "download_icon_full_path", "pdf_full_path"];
+
+    public $translatedAttributes = ["title", "excerpt", "learn_more", "single_page_title", "about_title", "about_description", "publications_title", "services_title", "contact_us_title", "pdf", "contact_us", "phone_text", "fax_text", "ext_text", "download_pdf", "different_section_image", "different_page_title", "different_section_description", "different_perform_transactions", "different_transactions_pdf", "different_contact_title", "different_pdf_btn"];
 
 
-    public function getIconFullPathAttribute(){
-        {
+    public function getIconFullPathAttribute()
+    { {
             if (isset($this->icon)) {
                 $icon_full_path = Helper::fullPath($this->icon);
                 return $icon_full_path;
@@ -32,8 +33,8 @@ class ServicesList extends Model  implements TranslatableContract
         }
     }
 
-    public function getGoldIconFullPathAttribute(){
-        {
+    public function getGoldIconFullPathAttribute()
+    { {
             if (isset($this->gold_icon)) {
                 $gold_icon_full_path = Helper::fullPath($this->gold_icon);
                 return $gold_icon_full_path;
@@ -43,8 +44,8 @@ class ServicesList extends Model  implements TranslatableContract
         }
     }
 
-    public function getImageFullPathAttribute(){
-        {
+    public function getImageFullPathAttribute()
+    { {
             if (isset($this->image)) {
                 $image_full_path = Helper::fullPath($this->image);
                 return $image_full_path;
@@ -54,8 +55,8 @@ class ServicesList extends Model  implements TranslatableContract
         }
     }
 
-    public function getPhoneIconFullPathAttribute(){
-        {
+    public function getPhoneIconFullPathAttribute()
+    { {
             if (isset($this->phone_icon)) {
                 $phone_icon_full_path = Helper::fullPath($this->phone_icon);
                 return $phone_icon_full_path;
@@ -65,8 +66,8 @@ class ServicesList extends Model  implements TranslatableContract
         }
     }
 
-    public function getFaxIconFullPathAttribute(){
-        {
+    public function getFaxIconFullPathAttribute()
+    { {
             if (isset($this->fax_icon)) {
                 $fax_icon_full_path = Helper::fullPath($this->fax_icon);
                 return $fax_icon_full_path;
@@ -76,8 +77,8 @@ class ServicesList extends Model  implements TranslatableContract
         }
     }
 
-    public function getExtIconFullPathAttribute(){
-        {
+    public function getExtIconFullPathAttribute()
+    { {
             if (isset($this->ext_icon)) {
                 $ext_icon_full_path = Helper::fullPath($this->ext_icon);
                 return $ext_icon_full_path;
@@ -87,8 +88,8 @@ class ServicesList extends Model  implements TranslatableContract
         }
     }
 
-    public function getMailIconFullPathAttribute(){
-        {
+    public function getMailIconFullPathAttribute()
+    { {
             if (isset($this->mail_icon)) {
                 $mail_icon_full_path = Helper::fullPath($this->mail_icon);
                 return $mail_icon_full_path;
@@ -98,8 +99,8 @@ class ServicesList extends Model  implements TranslatableContract
         }
     }
 
-    public function getDownloadIconFullPathAttribute(){
-        {
+    public function getDownloadIconFullPathAttribute()
+    { {
             if (isset($this->download_icon)) {
                 $download_icon_full_path = Helper::fullPath($this->download_icon);
                 return $download_icon_full_path;
@@ -109,8 +110,8 @@ class ServicesList extends Model  implements TranslatableContract
         }
     }
 
-    public function getPdfFullPathAttribute(){
-        {
+    public function getPdfFullPathAttribute()
+    { {
             if (isset($this->pdf)) {
                 $pdf_full_path = Helper::fullPath($this->pdf);
                 return $pdf_full_path;
@@ -120,7 +121,20 @@ class ServicesList extends Model  implements TranslatableContract
         }
     }
 
-	public function related_publications() { return $this->belongsToMany('App\PublicationsList', 'publications_list_services_list', 'services_list_id', 'publications_list_id')->orderBy('publications_list_services_list.ht_pos'); } public function related_services() { return $this->belongsToMany('App\ServicesList', 'services_list_services_list', 'services_list_id', 'other_services_list_id')->orderBy('services_list_services_list.ht_pos'); } 
-	public function e_services() { return $this->belongsToMany('App\EService', 'e_service_services_list', 'services_list_id', 'e_service_id')->orderBy('e_service_services_list.ht_pos'); }
-	public function locations() { return $this->belongsToMany('App\Location', 'location_services_list', 'services_list_id', 'location_id')->orderBy('location_services_list.ht_pos'); }
+    public function related_publications()
+    {
+        return $this->belongsToMany('App\PublicationsList', 'publications_list_services_list', 'services_list_id', 'publications_list_id')->orderBy('publications_list_services_list.ht_pos');
+    }
+    public function related_services()
+    {
+        return $this->belongsToMany('App\ServicesList', 'services_list_services_list', 'services_list_id', 'other_services_list_id')->orderBy('services_list_services_list.ht_pos');
+    }
+    public function e_services()
+    {
+        return $this->belongsToMany('App\EService', 'e_service_services_list', 'services_list_id', 'e_service_id')->orderBy('e_service_services_list.ht_pos');
+    }
+    public function locations()
+    {
+        return $this->belongsToMany('App\Location', 'location_services_list', 'services_list_id', 'location_id')->orderBy('location_services_list.ht_pos');
+    }
 }
