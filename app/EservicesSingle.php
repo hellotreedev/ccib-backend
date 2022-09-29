26d7c66861cda1a9bfce254ad93383a0c9086405
hellotreedigital <info@hellotree.co>
2022-09-29 13:18:15 +0300

    added btn pdf full path

--- app/EservicesSingle.php
+++ app/EservicesSingle.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Helpers\Helper;
 use Illuminate\Database\Eloquent\Model;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract; use Astrotomic\Translatable\Translatable;
 
@@ -15,7 +16,19 @@ class EservicesSingle extends Model  implements TranslatableContract
 
     protected $hidden = ['translations'];
 
+    public $appends = ['btn_pdf_full_path'];
+
     public $translatedAttributes = ["title","subtitle","section1_title","section1_excerpt","section1_btn","section2_content","section3_title","section4_content","section5_content","download_btn"];
+
+    public function getBtnPdfFullPathAttribute()
+    {
+        if (isset($this->download_btn_pdf)) {
+            $download_btn_pdf_full_path = Helper::fullPath($this->download_btn_pdf);
+            return $download_btn_pdf_full_path;
+        } else {
+            return null;
+        }
+    }
 
 	public function boxes() { return $this->belongsToMany('App\EservicesBox', 'eservices_box_eservices_single', 'eservices_single_id', 'eservices_box_id')->orderBy('eservices_box_eservices_single.ht_pos'); } 
 	
