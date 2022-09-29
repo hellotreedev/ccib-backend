6c651b204a06447236893a6113500d0bfbd80fcb
hellotreedigital <info@hellotree.co>
2022-09-29 11:25:43 +0300

    added read more to translations

--- app/NewsSetting.php
+++ app/NewsSetting.php
@@ -13,9 +13,7 @@ class NewsSetting extends Model  implements TranslatableContract
 
     protected $guarded = ['id'];
 
-    protected $hidden = ['translations'];
-
-    public $translatedAttributes = ["page_title","search_placeholder","search_btn","year_placeholder","category_placeholder", "learn_more", "gallery_title", "pdf_label"];
+    public $translatedAttributes = ["page_title","search_placeholder","search_btn","year_placeholder","category_placeholder","learn_more","gallery_title","pdf_label","read_more"];
 
 	
 }
