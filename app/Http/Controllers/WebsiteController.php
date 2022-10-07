1e6348bbaaa6087d0b0baf772c9524c155176012
hellotreedigital <info@hellotree.co>
2022-10-07 14:39:45 +0300

    added dynamic sitemap

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\ProjectList;
-use App\ServiceList;
+use App\EservicesSingle;
+use App\Event;
+use App\NewsList;
+use App\ServicesList;
+use Carbon\Carbon;
+use App\DirectoryList;
+use App\Project;
 use Illuminate\Http\Request;
 
 class WebsiteController extends Controller
@@ -11,7 +16,16 @@ class WebsiteController extends Controller
     public function sitemap()
     {
         return response()
-            ->view('sitemap')
+            ->view('sitemap', [
+                'services'=>ServicesList::select('slug')->get(),
+                'e_services'=>EservicesSingle::select('slug')->get(),
+                'news'=>NewsList::select('slug')->get(),
+                'past_events'=>Event::where("date", "<=", Carbon::now())->select('slug')->get(),
+                'upcoming_events'=>Event::where("date", ">", Carbon::now())->select('slug')->get(),
+                'single_directory'=>DirectoryList::select('slug')->get(),
+                'projects'=>Project::select('slug')->get()
+                
+            ])
             ->header('Content-Type', 'text/xml');
     }
 }
--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,7 +1,105 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-        <loc>{{request()->getSchemeAndHttpHost()}}</loc>
+        <loc>{{env("WEBSITE_URL")}}</loc>
         <lastmod>{{ date('Y-m-d') }}</lastmod>
     </url>
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'about-us'}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'services'}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'membership-directory'}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'contact-us'}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'publications'}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'news'}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'prev-events'}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'upcoming-events'}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'internal-projects'}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'news-and-events'}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'chambers-law'}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'board-of-directors'}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'our-sponsors'}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'e-services'}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    @foreach ($services as $service)
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'services/'.$service->slug}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    @endforeach
+    @foreach ($e_services as $eservice)
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'e-services/'.$eservice->slug}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    @endforeach
+    @foreach ($news as $single_news)
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'news/'.$single_news->slug}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    @endforeach
+    @foreach ($past_events as $event)
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'prev-events/'.$event->slug}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    @endforeach
+    @foreach ($upcoming_events as $event)
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'upcoming-events/'.$event->slug}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    @endforeach
+    @foreach ($single_directory as $dir)
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'membership-directory/'.$dir->slug}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    @endforeach
+    @foreach ($projects as $project)
+    <url>
+        <loc>{{env("WEBSITE_URL"). 'international-projects/'.$project->slug}}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+    </url>
+    @endforeach
 </urlset>
