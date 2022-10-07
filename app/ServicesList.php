9aea1451c5748f0e5fa78095a29a5716e0cf937a
hellotreedigital <info@hellotree.co>
2022-10-07 12:58:08 +0300

    added services icon to settings

--- app/ServicesList.php
+++ app/ServicesList.php
@@ -17,7 +17,7 @@ class ServicesList extends Model  implements TranslatableContract
 
     protected $hidden = ['translations'];
 
-    public $appends = ["image_full_path", "icon_full_path", "gold_icon_full_path", "phone_icon_full_path", "fax_icon_full_path", "ext_icon_full_path", "mail_icon_full_path", "download_icon_full_path", "pdf_full_path"];
+    public $appends = ["image_full_path", "icon_full_path", "gold_icon_full_path", "pdf_full_path"];
 
     public $translatedAttributes = ["title", "excerpt", "learn_more", "single_page_title", "about_title", "about_description", "publications_title", "services_title", "contact_us_title", "pdf", "contact_us", "phone_text", "fax_text", "ext_text", "download_pdf", "different_section_image", "different_page_title", "different_section_description", "different_perform_transactions", "different_transactions_pdf", "different_contact_title", "different_pdf_btn"];
 
@@ -55,60 +55,7 @@ class ServicesList extends Model  implements TranslatableContract
         }
     }
 
-    public function getPhoneIconFullPathAttribute()
-    { {
-            if (isset($this->phone_icon)) {
-                $phone_icon_full_path = Helper::fullPath($this->phone_icon);
-                return $phone_icon_full_path;
-            } else {
-                return null;
-            }
-        }
-    }
-
-    public function getFaxIconFullPathAttribute()
-    { {
-            if (isset($this->fax_icon)) {
-                $fax_icon_full_path = Helper::fullPath($this->fax_icon);
-                return $fax_icon_full_path;
-            } else {
-                return null;
-            }
-        }
-    }
-
-    public function getExtIconFullPathAttribute()
-    { {
-            if (isset($this->ext_icon)) {
-                $ext_icon_full_path = Helper::fullPath($this->ext_icon);
-                return $ext_icon_full_path;
-            } else {
-                return null;
-            }
-        }
-    }
-
-    public function getMailIconFullPathAttribute()
-    { {
-            if (isset($this->mail_icon)) {
-                $mail_icon_full_path = Helper::fullPath($this->mail_icon);
-                return $mail_icon_full_path;
-            } else {
-                return null;
-            }
-        }
-    }
-
-    public function getDownloadIconFullPathAttribute()
-    { {
-            if (isset($this->download_icon)) {
-                $download_icon_full_path = Helper::fullPath($this->download_icon);
-                return $download_icon_full_path;
-            } else {
-                return null;
-            }
-        }
-    }
+    
 
     public function getPdfFullPathAttribute()
     { {
--- app/ServicesSetting.php
+++ app/ServicesSetting.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Helpers\Helper;
 use Illuminate\Database\Eloquent\Model;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract; use Astrotomic\Translatable\Translatable;
 
@@ -15,8 +16,62 @@ class ServicesSetting extends Model  implements TranslatableContract
 
     protected $hidden = ['translations'];
 
+    public $appends = ["phone_icon_full_path", "fax_icon_full_path", "ext_icon_full_path", "mail_icon_full_path", "download_icon_full_path"];
 
     public $translatedAttributes = ["page_title","e_services_title"];
 
-	
+	public function getPhoneIconFullPathAttribute()
+    { {
+            if (isset($this->phone_icon)) {
+                $phone_icon_full_path = Helper::fullPath($this->phone_icon);
+                return $phone_icon_full_path;
+            } else {
+                return null;
+            }
+        }
+    }
+
+    public function getFaxIconFullPathAttribute()
+    { {
+            if (isset($this->fax_icon)) {
+                $fax_icon_full_path = Helper::fullPath($this->fax_icon);
+                return $fax_icon_full_path;
+            } else {
+                return null;
+            }
+        }
+    }
+
+    public function getExtIconFullPathAttribute()
+    { {
+            if (isset($this->ext_icon)) {
+                $ext_icon_full_path = Helper::fullPath($this->ext_icon);
+                return $ext_icon_full_path;
+            } else {
+                return null;
+            }
+        }
+    }
+
+    public function getMailIconFullPathAttribute()
+    { {
+            if (isset($this->mail_icon)) {
+                $mail_icon_full_path = Helper::fullPath($this->mail_icon);
+                return $mail_icon_full_path;
+            } else {
+                return null;
+            }
+        }
+    }
+
+    public function getDownloadIconFullPathAttribute()
+    { {
+            if (isset($this->download_icon)) {
+                $download_icon_full_path = Helper::fullPath($this->download_icon);
+                return $download_icon_full_path;
+            } else {
+                return null;
+            }
+        }
+    }
 }
