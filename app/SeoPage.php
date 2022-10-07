68892efe3bb4e69031bd65aa3abee941525dff94
hellotree.dev <support@hellotree.dev>
2022-10-07 12:01:33 +0300

    seo

--- app/SeoPage.php
+++ app/SeoPage.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App;
+use App\Helpers\Helper;
 
 use Illuminate\Database\Eloquent\Model;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract; use Astrotomic\Translatable\Translatable;
@@ -14,8 +15,19 @@ class SeoPage extends Model  implements TranslatableContract
     protected $guarded = ['id'];
     
     protected $hidden = ['translations'];
+    
+    public $appends = ['image_full_path'];
 
     public $translatedAttributes = ["title","description"];
-
+    
+     public function getImageFullPathAttribute()
+    {
+        if (isset($this->image)) {
+            $full_path_image = Helper::fullPath($this->image);
+            return $full_path_image;
+        } else {
+            return null;
+        }
+    }
 	
 }
