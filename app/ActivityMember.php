a5ae345b8cc5becaae7c12880110ef268a3d4912
hellotreedigital <info@hellotree.co>
2022-10-07 12:00:40 +0300

    added unified icons inside directory settings

--- app/ActivityMember.php
+++ app/ActivityMember.php
@@ -20,7 +20,7 @@ class ActivityMember extends Model  implements TranslatableContract
 
     protected $hidden = ['translations'];
 
-    public $appends = ['image_full_path', 'phone_icon_full_path', 'fax_icon_full_path', 'mail_icon_full_path', 'web_icon_full_path', 'loc_icon_full_path'];
+    public $appends = ['image_full_path'];
 
     protected $searchable = [
         'columns' => [
@@ -42,55 +42,7 @@ class ActivityMember extends Model  implements TranslatableContract
         }
     }
 
-    public function getPhoneIconFullPathAttribute()
-    {
-        if (isset($this->phone_icon)) {
-            $full_path_image = Helper::fullPath($this->phone_icon);
-            return $full_path_image;
-        } else {
-            return null;
-        }
-    }
-
-    public function getFaxIconFullPathAttribute()
-    {
-        if (isset($this->fax_icon)) {
-            $full_path_image = Helper::fullPath($this->fax_icon);
-            return $full_path_image;
-        } else {
-            return null;
-        }
-    }
-
-    public function getMailIconFullPathAttribute()
-    {
-        if (isset($this->mail_icon)) {
-            $full_path_image = Helper::fullPath($this->mail_icon);
-            return $full_path_image;
-        } else {
-            return null;
-        }
-    }
-
-    public function getWebIconFullPathAttribute()
-    {
-        if (isset($this->web_icon)) {
-            $web_icon_full_path = Helper::fullPath($this->web_icon);
-            return $web_icon_full_path;
-        } else {
-            return null;
-        }
-    }
-
-    public function getLocIconFullPathAttribute()
-    {
-        if (isset($this->loc_icon)) {
-            $loc_icon_full_path = Helper::fullPath($this->loc_icon);
-            return $loc_icon_full_path;
-        } else {
-            return null;
-        }
-    }
+   
 
 
     public $translatedAttributes = ["title","description", "learn_more", "popup_description", "contact", "phone1_text", "phone2_text", "phone3_text", "fax_text", "location_text"];
--- app/Directory.php
+++ app/Directory.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Helpers\Helper;
 use Illuminate\Database\Eloquent\Model;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract; use Astrotomic\Translatable\Translatable;
 
@@ -15,8 +16,61 @@ class Directory extends Model  implements TranslatableContract
 
     protected $hidden = ['translations'];
 
+    public $appends = ['phone_icon_full_path', 'fax_icon_full_path', 'mail_icon_full_path', 'web_icon_full_path', 'loc_icon_full_path'];
 
     public $translatedAttributes = ["page_title","section1_title","section1_description","section2_title", "view_all"];
+
+    public function getPhoneIconFullPathAttribute()
+    {
+        if (isset($this->phone_icon)) {
+            $full_path_image = Helper::fullPath($this->phone_icon);
+            return $full_path_image;
+        } else {
+            return null;
+        }
+    }
+
+    public function getFaxIconFullPathAttribute()
+    {
+        if (isset($this->fax_icon)) {
+            $full_path_image = Helper::fullPath($this->fax_icon);
+            return $full_path_image;
+        } else {
+            return null;
+        }
+    }
+
+    public function getMailIconFullPathAttribute()
+    {
+        if (isset($this->mail_icon)) {
+            $full_path_image = Helper::fullPath($this->mail_icon);
+            return $full_path_image;
+        } else {
+            return null;
+        }
+    }
+
+    public function getWebIconFullPathAttribute()
+    {
+        if (isset($this->web_icon)) {
+            $web_icon_full_path = Helper::fullPath($this->web_icon);
+            return $web_icon_full_path;
+        } else {
+            return null;
+        }
+    }
+
+    public function getLocIconFullPathAttribute()
+    {
+        if (isset($this->loc_icon)) {
+            $loc_icon_full_path = Helper::fullPath($this->loc_icon);
+            return $loc_icon_full_path;
+        } else {
+            return null;
+        }
+    }
+
+
 
     
 }
