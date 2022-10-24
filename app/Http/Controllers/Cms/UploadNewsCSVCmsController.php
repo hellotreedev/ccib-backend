f791dca629fc34347f4ff7b7007ff7d2b86a0ace
hellotreedigital <info@hellotree.co>
2022-10-24 18:04:09 +0300

    fixing more news

--- app/Http/Controllers/Cms/UploadNewsCSVCmsController.php
+++ app/Http/Controllers/Cms/UploadNewsCSVCmsController.php
@@ -194,9 +194,9 @@ class UploadNewsCSVCmsController extends Controller
                 $news->save();
                 $news->news_categories()->sync($news_categories);
 
-                if($more_news){
-                    $news->more_news()->sync($more_news);
-                }
+                // if($more_news){
+                //     $news->more_news()->sync($more_news);
+                // }
 
                 if ($singleNews[25]) {
                     $gallery = $singleNews[25];
