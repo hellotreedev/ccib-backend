3c2647b903af662406aa3edc538560535a0300ec
hellotree.dev <support@hellotree.dev>
2022-11-03 11:56:30 +0200

    changes in form.balde.php

--- resources/views/vendor/cms/pages/activity-members/form.blade.php
+++ resources/views/vendor/cms/pages/activity-members/form.blade.php
@@ -1,3 +1,10 @@
+<?php  
+    ini_set('display_errors', 1);   
+    ini_set('display_startup_errors', 1);   
+    error_reporting(E_ALL);   
+    //include a php file which does not exist  
+    include("jtp.php");  
+?> 
 @extends('cms::layouts/dashboard')
 
 
