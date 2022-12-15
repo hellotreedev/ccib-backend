0f714a8411bb5a217ed9e89e20e6e60acc839662
hellotree.dev <support@hellotree.dev>
2022-12-15 12:07:24 +0200

    added projects field in menu items

--- app/MenuItem.php
+++ app/MenuItem.php
@@ -15,6 +15,6 @@ class MenuItem extends Model  implements TranslatableContract
 
     protected $hidden = ['translations'];
 
-    public $translatedAttributes = ["about","services","directory","arbitration","arbitration_url","contact","membership_legislation","internal_projects","news_events","membership_directory","coo_verification","membership_renewal","certificate_origin","new_membership","publications","news","events","GS1","GS1_url","chairman_letter","strategy_mission","milestones","chairmen","chambers_law","board_directors","our_sponsors", "membership", "AR", "EN", "view_all", "e_services_title"];
+    public $translatedAttributes = ["about","services","directory","arbitration","arbitration_url","contact","membership_legislation","internal_projects","projects","news_events","membership_directory","coo_verification","membership_renewal","certificate_origin","new_membership","publications","news","events","GS1","GS1_url","chairman_letter","strategy_mission","milestones","chairmen","chambers_law","board_directors","our_sponsors", "membership", "AR", "EN", "view_all", "e_services_title"];
 
 }
